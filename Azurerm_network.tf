2695c0e76eb933cdbee9cf2e0eac690cb709c520
amaurybox <amaurybox@gmail.com>
2023-03-03 11:56:09 +0100

    Ajout du network + apply

--- Azurerm_network.tf
+++ Azurerm_network.tf
@@ -1,15 +1,9 @@
 
 resource "azurerm_virtual_network" "GroupeB" {
-  name                = "vnet-GroupeB" # to change by Groupe ....
+  name                = "vnet-GroupeB" 
   resource_group_name = azurerm_resource_group.GroupeB.name
   location            = azurerm_resource_group.GroupeB.location
-  address_space       = ["20.0.0.0/16"] # to change by Groupe ....
-
-  tags = {
-    environment = "Ecole_CAP_Azure"
-    session     = "1"
-    groupe      = "B"
-  }
+  address_space       = ["20.0.0.0/16"] 
 }
 
 resource "azurerm_subnet" "GroupeB" {
@@ -17,10 +11,5 @@ resource "azurerm_subnet" "GroupeB" {
   resource_group_name  = azurerm_resource_group.GroupeB.name
   virtual_network_name = azurerm_virtual_network.GroupeB.name
   address_prefixes     = ["20.0.2.0/24"] 
-
-    tags = {
-    environment = "Ecole_CAP_Azure"
-    session     = "1"
-    groupe      = "B"
-  }
 }
+
