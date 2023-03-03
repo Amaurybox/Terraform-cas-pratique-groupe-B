962ac2a77706e74c570895724f12621ce814e74e
amaurybox <amaurybox@gmail.com>
2023-03-03 09:22:56 +0100

    Add Provider

---
+++ Azurerm_provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  # subscription_id : "Ecole By Cap"
+  subscription_id = "a177ac1b-f0e1-45e5-a6c4-80266ff85e1d"
+}
+
+data "azurerm_resource_group" "GroupeN" {
+  name = "GroupeB" # change by Groupe1/Groupe2/...
+
+}
+
+
+
+
+
+
